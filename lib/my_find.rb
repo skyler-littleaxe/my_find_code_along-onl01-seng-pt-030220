f66b6782b83f6582138497ef38bcf3fdf3fdd3ac
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-13 02:48:48 +0000

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,13 @@
-require 'pry'
+#require 'pry'
 
 def my_find(collection)
-
+  i = 0
+while i < collection.length 
+ #if yield(collection[i])
+   return collection [i] if yield(collection[i])
+i += 1
 end
+end
+
+collection = (1..100).to_a
+my_find(collection) {|i| i % 3 == 0 and % 5 == 0}
