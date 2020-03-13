148aa67b9df6451788853c87ad90be6b30ada1c6
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-13 02:47:38 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,11 @@
 def my_find(collection)
   i = 0
 while i < collection.length 
- if yield(collection[i])
-   return collection [i]
- end
+ #if yield(collection[i])
+   return collection [i] if yield(collection[i])
 i += 1
 end
 end
+
+collection = (1..100).to_a
+my_find(collection) {|i| i % 3 == 0 and % 5 == 0}
